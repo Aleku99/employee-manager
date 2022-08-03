b4a7ffcfe0a191232cab99a9656c6168bb472e32
DeeaMG <55800409+DeeaMG@users.noreply.github.com>
2022-08-03 11:41:48 +0300

    Created user reports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import UserSkills from "./pages/UserSkills/UserSkills";
+import UserList from "./pages/UserList/UserList";
+import ReportScreen from "./pages/ReportScreen/ReportScreen";
 
 function App() {
   return (
